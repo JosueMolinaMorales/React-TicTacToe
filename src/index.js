97eb3b32a1eefd57058d02d158635e38aa01c89b
Josue Morales <molinajosue92@hotmail.com>
2022-01-16 17:13:19 -0500

    Finished tutorial

--- src/index.js
+++ src/index.js
@@ -67,6 +67,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true
         };
     }
@@ -76,6 +77,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         });
     }
@@ -87,7 +89,7 @@ class Game extends React.Component {
     * @returns 
     */
      handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if(calculateWinner(squares) || calculateTie(squares) || squares[i]) {
@@ -98,7 +100,15 @@ class Game extends React.Component {
             history: history.concat([{
                 squares:squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
@@ -108,9 +118,21 @@ class Game extends React.Component {
     
     render() {
         const history = this.state.history;
-        const current = history[history.length -1];
+        const current = history[this.state.stepNumber];
         const isTied = calculateTie(current.squares);
         const winner = calculateWinner(current.squares); 
+
+        const moves = history.map((step,move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if(winner) {
             status = 'Winner: ' + winner;
@@ -130,7 +152,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                        <ol>{/* TODO */}</ol>
+                        <ol>{moves}</ol>
                 </div>
                 <div hidden={!winner && !isTied}>
                     {this.renderPlayAgainButton()}
